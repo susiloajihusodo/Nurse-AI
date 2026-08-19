497fd5bedc28c3fbddb20392f0f0510c5a49a581
susiloajihusodo <75867299+susiloajihusodo@users.noreply.github.com>
2026-08-19 17:42:41 +0700

    Delete extract_labels.ps1

--- extract_labels.ps1
+++
@@ -1,9 +0,0 @@
-$lines = Get-Content "C:\Users\susilo\AppData\Local\Programs\Microsoft VS Code\nursing-assessment\js\sdki-db.js"
-$labels = @()
-for ($i = 0; $i -lt $lines.Length; $i++) {
-    if ($lines[$i] -match '^\s+label:') {
-        $label = $lines[$i] -replace '^\s+label:\s*"', '' -replace '",?\s*$', ''
-        $labels += $label
-    }
-}
-$labels | Sort-Object -Unique
